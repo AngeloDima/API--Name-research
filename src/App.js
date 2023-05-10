7ae32d9df94145fd3ff98f14069c41d9a268c10d
Angelo Di mauro <angelodimauro91@gmail.com>
2023-05-10 20:48:09 +0200

    end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [ricerca, setRicerca] = useState('');
+  const [checkItem, setCheckItem] = useState({});
+
+  useEffect(() => {
+    fetch('https://raintonic.com/careers/es5_dataset.json')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  const ricercaUtente = event => {
+    setRicerca(event.target.value);
+  };
+
+  const filtroData = data.filter(item => {
+    const fullName = `${item.first_name} ${item.last_name}`.toLowerCase();
+    return fullName.includes(ricerca.toLowerCase());
+  });
+
+
+
+
+  
+  // inizializza lo stato delle checkbox con i valori salvati nel localStorage solo la prima volta che l'app viene caricata
+  useEffect(() => {
+    const initStore = JSON.parse(localStorage.getItem('checkItem')) || {};
+    if (Object.keys(initStore).length > 0) {
+      setCheckItem(initStore);
+    }
+  }, []);
+
+  // salva lo stato delle checkbox nel localStorage ogni volta che cambia
+  useEffect(() => {
+    localStorage.setItem('checkItem', JSON.stringify(checkItem));
+  }, [checkItem]);
+
+  // gestisce il cambio di stato della checkbox
+  const changeState = event => {
+    const { id, checked } = event.target;
+    setCheckItem(prevState => ({ ...prevState, [id]: checked }));
+  };
+
+
+
+
+
+  // filtra i contatti preferiti
+  const favoritiFilter = filtroData.filter(item => checkItem[item.id]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={ricerca} onChange={ricercaUtente} />
+      <ol>
+        {filtroData.map(item => (
+          <li key={item.id}>
+            {item.first_name} {item.last_name}
+            <input
+              type="checkbox"
+              name=""
+              id={item.id}
+              checked={checkItem[item.id]}
+              onChange={changeState}
+            />
+          </li>
+        ))}
+      </ol>
+      <h2>Preferiti:</h2>
+      <ol>
+        {favoritiFilter.map(item => (
+          <li key={item.id}>
+            {item.first_name} {item.last_name}
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
 
 export default App;
+
+
